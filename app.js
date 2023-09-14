852539c79b276ca3f50cab86faec4bef34ca1a86
Christopher Alley <calleyviola@gmail.com>
2023-09-14 14:34:48 -0400

    Connect up js files

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
+"use strict";
+
 /** BizTime express application. */
 
 const express = require("express");
 const { NotFoundError } = require("./expressError");
+const companiesRoutes = require("./routes/companies");
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/companies", companiesRoutes);
 
 
 /** 404 handler: matches unmatched routes; raises NotFoundError. */
