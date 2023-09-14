296e4d6bba1641995299fdc8e60c7329e6df63dd
Christopher Alley <calleyviola@gmail.com>
2023-09-14 19:47:49 -0400

    Add test for get /companies

--- routes/companies.test.js
+++ routes/companies.test.js
@@ -1,6 +1,28 @@
 "use strict";
 
-const db = require("../db")
-const app = require("../app")
-const request = require("supertest")
+const db = require("../db");
+const app = require("../app");
+const request = require("supertest");
 
+let testCompany;
+
+beforeEach(async function () {
+  await db.query("DELETE FROM companies");
+  let result = await db.query(`
+    INSERT INTO companies (code, name, description)
+    VALUES ('testCode', 'testCompany', 'an amazing company')
+    RETURNING code, name, description`);
+  testCompany = result.rows[0];
+});
+
+
+/** GET /companies - returns {companies: [{code, name}, ...]} */
+
+describe("GET /companies", function () {
+  test("Gets companies", async function () {
+    const resp = await request(app).get('/companies');
+    expect(resp.body).toEqual({
+      companies: [{ code: testCompany.code, name: testCompany.name }],
+    });
+  });
+});
