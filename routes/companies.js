b75e0a0747f989599486573460c6a5270a2a2bfe
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2023-09-14 12:03:06 -0700

    added get /code and post routes in companies

--- routes/companies.js
+++ routes/companies.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const express = require("express");
+const { NotFoundError, BadRequestError } = require("../expressError");
 
 const db = require("../db");
 const router = new express.Router();
@@ -12,6 +13,37 @@ router.get("/", async function (req, res) {
   );
   const companies = results.rows;
   return res.json({ companies });
+});
+
+router.get("/:code", async function (req, res) {
+  const code = req.params.code;
+
+  const results = await db.query(
+    `SELECT code, name, description
+      FROM companies
+      WHERE code = $1`, [code]
+  );
+
+  const company = results.rows[0];
+
+  if (company === undefined) throw new NotFoundError("Company is not found.");
+
+  return res.json({ company });
+});
+
+router.post("/", async function (req, res) {
+  if (req.body === undefined) throw new BadRequestError();
+  //FIXME: conditional is not being hit when request body is empty
+
+  const { code, name, description } = req.body;
+  const results = await db.query(
+    `INSERT INTO companies (code, name, description)
+      VALUES ($1, $2, $3)
+      RETURNING code, name, description`,
+      [code, name, description]
+  );
+  const company = results.rows[0];
+  return res.status(201).json({ company });
 });
 
 
