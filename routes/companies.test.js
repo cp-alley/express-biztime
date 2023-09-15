ed42eaaa13934da40697a0d9a03f71a962217447
John Almodovar <124217251+johnalmodovar@users.noreply.github.com>
2023-09-14 17:10:38 -0700

    completed test routes for Get single company, POST company, PUT company

--- routes/companies.test.js
+++ routes/companies.test.js
@@ -26,3 +26,90 @@ describe("GET /companies", function () {
     });
   });
 });
+
+/** GET /companies/:code - return data about one company:
+ * {code, name, description, invoices: [id,...]} */
+
+describe("GET /companies/:code", function () {
+  test("Gets single company", async function () {
+    const resp = await request(app).get(`/companies/${testCompany.code}`);
+    expect(resp.body).toEqual({
+       company: {
+        code: testCompany.code,
+        name: testCompany.name,
+        description: testCompany.description,
+        invoices: []
+      }
+    });
+  });
+});
+
+/** POST /companies - create company from data;
+ * return `{company: {code, name, description}}` */
+
+describe("POST /companies", function () {
+  test("Create new company", async function () {
+    const resp = await request(app)
+      .post("/companies")
+      .send({
+        code: "test2",
+        name: "test2name",
+        description: "another amazing company"
+       });
+    expect(resp.statusCode).toEqual(201);
+    expect(resp.body).toEqual({
+      company: {
+        code: "test2",
+        name: "test2name",
+        description: "another amazing company"
+      }
+    });
+  });
+
+  test("400 if empty request body", async function () {
+    const resp = await request(app)
+        .post("/companies")
+        .send();
+    expect(resp.statusCode).toEqual(400);
+  });
+});
+
+/** PUT /companies/:code - updates company;
+ * returns `{company: {code, name, description}}`
+ */
+
+describe("PUT /companies/:code", function () {
+  test("Update single company", async function () {
+    const resp = await request(app)
+        .put(`/companies/${testCompany.code}`)
+        .send({
+          name: "Yes",
+          description: "Yes"
+        });
+    expect(resp.statusCode).toEqual(200);
+    expect(resp.body).toEqual({
+      company: {
+        code: testCompany.code,
+        name: "Yes",
+        description: "Yes"
+      }
+    });
+  });
+
+  test("404 if not found", async function () {
+    const resp = await request(app)
+        .put(`/companies/99`)
+        .send({
+          name: "Yes",
+          description: "Yes"
+        });
+    expect(resp.statusCode).toEqual(404);
+  });
+
+  test("400 if empty body request", async function () {
+    const resp = await request (app)
+        .put(`/companies/${testCompany.code}`)
+        .send();
+    expect(resp.statusCode).toEqual(400);
+  });
+});
